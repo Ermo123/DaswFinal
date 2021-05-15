21ca0bbceb7fe27f558c5a05930b895b7c144768
Leon <44817515+Ermo123@users.noreply.github.com>
2021-05-15 14:45:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const usuarioRouter = require('./routes/usuario-route');
 const authRouter = require('./routes/auth-route');
 const companiesRouter = require('./routes/companies-route');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
